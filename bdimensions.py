f473bc5ccbafab4199a9e809c224878b2c975b1b
Jori Mykkänen <jorimyk@hotmail.com>
2017-07-17 14:51:40 +0300

    Functionality for /users/<user>/data -level
    Complete POST, GET and DELETE functions for  /users/<user>/data -level

--- bdimensions.py
+++ bdimensions.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, abort, jsonify
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, deferred
 from models import Base, Users, UserData
 import json
 
@@ -25,7 +25,7 @@ def users():
                 d = request.get_json()
                 return addNewUser(d)
             else:
-                abort (400, 'No json in request')
+                abort (400, 'No json in request content')
         else:
             abort(415, 'Content Type must be application/json')
     elif request.method == 'GET': # List all users
@@ -36,41 +36,48 @@ def users():
         abort(501, 'Delete all users not imlemented')
 
 
-@app.route('/users/<int:user_id>', methods = ['GET', 'PUT', 'DELETE'])
-def user(user_id):
-    if request.method == 'GET': # Show user with user_id
-        return getUser(user_id)
+@app.route('/users/<int:userId>', methods = ['GET', 'PUT', 'DELETE'])
+def user(userId):
+    if request.method == 'GET': # Show user with userId
+        return getUser(userId)
     elif request.method == 'PUT':
         if request.headers['Content-Type'] == 'application/json':
             if request.get_json(silent=True):
                 d = request.get_json()
-                return updateUser(user_id, d)
+                return updateUser(userId, d)
             else:
-                abort (400, 'No json in request')
+                abort (400, 'No json in request content')
         else:
             abort(415, 'Content Type must be application/json')
     elif request.method == 'DELETE':
-        return deleteUser(user_id)
+        return deleteUser(userId)
 
 
-@app.route('/users/<int:user_id>/data', methods = ['POST', 'GET', 'DELETE'])
-def user_data(user_id):
+@app.route('/users/<int:userId>/data', methods = ['POST', 'GET', 'DELETE'])
+def user_data(userId):
     if request.method == 'POST':
-        return 'Add a new data item for user with id %s' % user_id
+        if request.headers['Content-Type'] == 'application/json':
+            if request.get_json(silent=True):
+                d = request.get_json()
+                return addNewData(userId, d)
+            else:
+                abort (400, 'No json in request content')
+        else:
+            abort(415, 'Content Type must be application/json')
     elif request.method == 'GET':
-        return 'List data for user with id %s' % user_id
+        return getUserData(userId)
     elif request.method == 'DELETE':
-        return 'Delete all data for user with id %s' % user_id
+        return deleteAllUserData(userId)
 
 
-@app.route('/users/<int:user_id>/data/<int:data_id>', methods = ['GET', 'PUT', 'DELETE'])
-def data(user_id, data_id):
+@app.route('/users/<int:userId>/data/<int:data_id>', methods = ['GET', 'PUT', 'DELETE'])
+def data(userId, data_id):
     if request.method == 'GET':
-        return 'Show data item with id %s for user with id %s' % (data_id, user_id)
+        return 'Show data item with id %s for user with id %s' % (data_id, userId)
     elif request.method == 'PUT':
-        return 'If exists update data item with id %s for user with id %s' % (data_id, user_id)
+        return 'If exists update data item with id %s for user with id %s' % (data_id, userId)
     elif request.method == 'DELETE':
-        return 'Delete data item with id %s for user with id %s' % (data_id, user_id)
+        return 'Delete data item with id %s for user with id %s' % (data_id, userId)
 
 
 def addNewUser(d):
@@ -91,49 +98,111 @@ def addNewUser(d):
 
 def getAllUsers():
     users = session.query(Users).all()
-    return jsonify(Users=[i.serialize for i in users])
+    if users:
+        return jsonify(Users=[i.serialize for i in users])
+    else:
+        abort(404, 'No users found')
 
-def getUser(user_id):
-    user = session.query(Users).filter_by(id = user_id).first()
+def getUser(userId):
+    user = session.query(Users).filter_by(id = userId).first()
     if user:
         return jsonify(Users=user.serialize)
     else:
-        abort(404, 'User with id %s does not exist' % user_id)
+        abort(404, 'User with id %s does not exist' % userId)
 
-def updateUser(user_id, d):
-    user = session.query(Users).filter_by(id = user_id).first()
+def updateUser(userId, d):
+    user = session.query(Users).filter_by(id = userId).first()
     if user:
-        i = 0
+        validData = False
         if 'firstName' in d and d['firstName']:
-            i += 1
+            validData = True
             user.firstName = d['firstName']
         if 'lastName' in d and d['lastName']:
-            i += 1
+            validData = True
             user.lastName = d['lastName']
         if 'genre' in d and d['genre']:
-            i += 1
+            validData = True
             user.genre = d['genre']
         if 'dateOfBirth' in d and d['dateOfBirth']:
-            i += 1
+            validData = True
             user.dateOfBirth = d['dateOfBirth']
-        if (i > 0):
+        if validData:
             session.add(user)
             session.commit()
             return jsonify(Users=user.serialize)
         else:
             abort(400, 'Request must contain firstName, lastName, genre or dateOfBirth keyword with values')
     else:
-        abort(404, 'User with id %s does not exist' % user_id)
+        abort(404, 'User with id %s does not exist' % userId)
 
 
-def deleteUser(user_id):
-    user = session.query(Users).filter_by(id = user_id).first()
+def deleteUser(userId):
+    user = session.query(Users).filter_by(id = userId).first()
     if user:
         session.delete(user)
         session.commit()
-        return 'Removed user with id %s' % user_id
+        return 'Removed user with id %s' % userId
     else:
-        abort(404, 'User with id %s does not exist' % user_id)
+        abort(404, 'User with id %s does not exist' % userId)
+
+
+def addNewData(userId, d):
+    user = session.query(Users).filter_by(id = userId).first()
+    if user:
+        if not 'height' in d: d['height'] = ''
+        if not 'weight' in d: d['weight'] = ''
+        if not 'waistline' in d: d['waistline'] = ''
+        if not 'fatTotal' in d: d['fatTotal'] = ''
+        if not 'bodyMass' in d: d['bodyMass'] = ''
+        if not 'fatVisceral' in d: d['fatVisceral'] = ''
+        if not 'measurementDate' in d or not d['measurementDate']:
+            abort(400, 'Request must contain keyword measurementDate with valid date')
+        #elif not 'height' in d and not 'weight' in d and not 'waistline' in d:
+            #abort(400, 'Request must contain height, weight or waistline keyword')
+        elif not d['height'] and not d['weight'] and not d['waistline']:
+            abort(400, 'Request must contain value for height, weight or waistline keyword')
+        elif (d['fatTotal'] or d['bodyMass'] or d['fatVisceral']) and not d['weight']:
+            abort(400, 'For fatTotal, bodyMass and fatVisceral request must also contain weight')
+        else:        
+            data = UserData( \
+            userId = userId, \
+            measurementDate = d['measurementDate'], \
+            height = d['height'], \
+            weight = d['weight'], \
+            waistline = d['waistline'], \
+            fatTotal = d['fatTotal'], \
+            bodyMass = d['bodyMass'], \
+            fatVisceral = d['fatVisceral'])
+            session.add(data)
+            session.commit()
+            return jsonify(UserData=data.serialize)
+    else:
+        abort(404, 'User with id %s does not exist' % userId)
+
+def getUserData(userId):
+    user = session.query(Users).filter_by(id = userId).first()
+    if user:
+        data = session.query(UserData).filter_by(userId = userId).all()
+        if data:
+            return jsonify(UserData=[i.serialize for i in data])
+        else:
+            abort(404, 'No data for user with id %s' % userId)
+    else:
+        abort(404, 'User with id %s does not exist' % userId)
+
+def deleteAllUserData(userId):
+    user = session.query(Users).filter_by(id = userId).first()
+    if user:
+        data = session.query(UserData).filter_by(userId = userId).all()
+        if data:
+            for index in range(len(data)):
+                session.delete(data[index])
+                session.commit()
+            return 'All %s data items removed for user with id %s' % (len(data), userId)
+        else:
+            abort(404, 'No data for user with id %s' % userId)            
+    else:
+        abort(404, 'User with id %s does not exist' % userId)
 
 
 if __name__ == '__main__':
