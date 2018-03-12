1aaf517d554ee224ae785c23826aba46489a0997
Jori Mykkänen <jori.mykkanen@gmail.com>
2018-03-12 20:14:17 +0200

    start using redesigned measurement data validation and measurement_exists function

--- bd_api/bd_api/users/measurements/views.py
+++ bd_api/bd_api/users/measurements/views.py
@@ -39,8 +39,8 @@ def measurements(userId):
 @app.route('/users/<int:userId>/data/<int:dataId>', methods = ['GET', 'PUT', 'DELETE']) # /users/<user>/data/<data>
 def measurement(userId, dataId):
     headers = request.headers
-    if not UserUtils.userExists(userId):
-        return jsonify(error='No user with id %s' % userId), 404
+    if not MeasurementUtils.measurement_exists(userId, dataId):
+        return jsonify(error='No data %s for user %s' % (dataId, userId)), 404
     elif request.method == 'GET' and UserUtils.checkIfPublicUser(userId):
         return getMeasurementItem(userId, dataId)
     elif request.authorization:
@@ -97,7 +97,6 @@ def addNewMeasurement(userId, d):
 
 def getMeasurements(userId):
     """Return all user measurements from database based on user id"""
-
     data = Measurement.query.filter_by(user_id = userId).all()
     if data:
         data = [i.serialize for i in data]
@@ -124,72 +123,59 @@ def deleteAllMeasurements(userId, internalCall):
 def getMeasurementItem(userId, dataId):
     """"Return single measurement item from database based on user id and data id"""
     data = Measurement.query.filter_by(user_id = userId).filter_by(id = dataId).first()
-    if data:
-        data = data.serialize
-        data['measurementDate'] = CommonUtils.convertToISODate(data['measurementDate'])
-        data['timestamp'] = CommonUtils.convertToISODate(data['timestamp'])
-        return jsonify(data)
-    else:
-        return jsonify(error='not found', userId=userId, username=getUsername(userId), dataId=dataId), 404
+    data = data.serialize
+    data['measurementDate'] = CommonUtils.convertToISODate(data['measurementDate'])
+    data['timestamp'] = CommonUtils.convertToISODate(data['timestamp'])
+    return jsonify(data)
 
 
 def updateMeasurementItem(userId, dataId, d):
     """Update user changes to database if valid values in d"""
     errorResponse = {'measurementDate': 'string YYYY-MM-DD (if key exists)', 'height': 'number/null (if key exists)', 'weight': 'number/null (if key exists)', 'waistline': 'number/null (if key exists)', 'fatTotal': 'number/null (if key exists)', 'bodyMass': 'number/null (if key exists)', 'fatVisceral': 'number/null (if key exists)'}
-    data = Measurement.query.filter_by(user_id = userId).filter_by(id = dataId).first()
-    if data:
-        if d:
-            validRequest = False
-            if 'measurementDate' in d and validateDate(userId, 'measurementDate', d.get('measurementDate')):
-                validRequest = True
-                data.measurementDate = CommonUtils.convertFromISODate(d.get('measurementDate'))
-            if 'height' in d and validateNumber('height', d.get('height')):
-                validRequest = True
-                data.height = d.get('height')
-            if 'weight' in d and validateNumber('weight', d.get('weight')):
-                validRequest = True
-                data.weight = d.get('weight')
-            if 'waistline' in d and validateNumber('waistline', d.get('waistline')):
-                validRequest = True
-                data.waistline = d.get('waistline')
-            if 'fatTotal' in d and validateNumber('fatTotal', d.get('fatTotal')):
-                validRequest = True
-                data.fatTotal = d.get('fatTotal')
-            if 'fatVisceral' in d and validateNumber('fatVisceral', d.get('fatVisceral')):
-                validRequest = True
-                data.fatVisceral = d.get('fatVisceral')
-            if 'bodyMass' in d and validateNumber('bodyMass', d.get('bodyMass')):
-                validRequest = True
-                data.bodyMass = d.get('bodyMass')
-            if not validRequest:
-                errorResponse['error'] = 'no valid keys or nothing to be updated'
-                return jsonify(errorResponse), 400
-            elif (d.get('fatTotal') or d.get('fatVisceral') or d.get('bodyMass')) and not data.weight and not d.get('weight'):
-                return jsonify(error='fatTotal, bodyMass and fatVisceral need value for weight', weight='Mandatory', fatTotal=d.get('fatTotal'), bodyMass=d.get('bodyMass'), fatVisceral=d.get('fatVisceral')), 400
+    if d:
+        if any(key in d for key in Measurement.measurement_keys):
+            if not MeasurementUtils.validate_measurement_values(d):
+                data = Measurement.query.filter_by(user_id = userId).filter_by(id = dataId).first()
+                if 'measurementDate' in d:
+                    data.measurementDate = CommonUtils.convertFromISODate(d.get('measurementDate'))
+                if 'height' in d:
+                    data.height = d.get('height')
+                if 'weight' in d:
+                    data.weight = d.get('weight')
+                if 'waistline' in d:
+                    data.waistline = d.get('waistline')
+                if 'fatTotal' in d:
+                    data.fatTotal = d.get('fatTotal')
+                if 'fatVisceral' in d:
+                    data.fatVisceral = d.get('fatVisceral')
+                if 'bodyMass' in d:
+                    data.bodyMass = d.get('bodyMass')
+                if (d.get('fatTotal') or d.get('fatVisceral') or d.get('bodyMass')) and not data.weight and not d.get('weight'):
+                    return jsonify(error='fatTotal, bodyMass and fatVisceral need value for weight', weight='Mandatory', fatTotal=d.get('fatTotal'), bodyMass=d.get('bodyMass'), fatVisceral=d.get('fatVisceral')), 400
+                else:
+                    db.session.add(data)
+                    db.session.commit()
+                    data = data.serialize
+                    data['measurementDate'] = CommonUtils.convertToISODate(data['measurementDate'])
+                    data['timestamp'] = CommonUtils.convertToISODate(data['timestamp'])
+                    response = jsonify(data)
+                    response.status_code = 201
+                    response.headers['Content-Location'] = '/users/' + str(userId) + '/data/' + str(data['id'])
+                    response.headers['Access-Control-Expose-Headers'] = 'Content-Location'
+                    return response
             else:
-                db.session.add(data)
-                db.session.commit()
-                data = data.serialize
-                data['measurementDate'] = CommonUtils.convertToISODate(data['measurementDate'])
-                data['timestamp'] = CommonUtils.convertToISODate(data['timestamp'])
-                response = jsonify(data)
-                response.status_code = 201
-                response.headers['Content-Location'] = '/users/' + str(userId) + '/data/' + str(data['id'])
-                response.headers['Access-Control-Expose-Headers'] = 'Content-Location'
-                return response
+                return jsonify(MeasurementUtils.validate_measurement_values(d)), 400
         else:
-            errorResponse['error'] = 'no valid JSON in request'
+            errorResponse['error'] = 'no valid keys or nothing to be updated'
             return jsonify(errorResponse), 400
     else:
-        return jsonify(error='measurement not found', dataId=dataId, userId=userId, username=getUsername(userId)), 404
+        errorResponse['error'] = 'no valid JSON in request'
+        return jsonify(errorResponse), 400
 
 
 def deleteMeasurementItem(userId, dataId):
     """Delete measurement item from database if user id and data id matches"""
     data = Measurement.query.filter_by(user_id = userId).filter_by(id = dataId).first()
-    if data:
-        db.session.delete(data)
-        db.session.commit()
-        return jsonify(result='measurement removed', userId=userId, username=getUsername(userId), dataId=dataId)
-    else:
-        return jsonify(error='not found', userId=userId, username=getUsername(userId), dataId=dataId), 404
+    db.session.delete(data)
+    db.session.commit()
+    return jsonify(result='measurement removed', userId=userId, username=getUsername(userId), dataId=dataId)
